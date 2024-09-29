3170886be0d468899351e7088404556ce4200233
Admin <Admin@DESKTOP-4VAU7A2>
2024-09-29 20:38:07 +0500

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task01
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.WriteLine("Я сойду и намечу");
+			Console.WriteLine();
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.WriteLine("Я сойду и намечу");
+			Console.WriteLine("Мой вечерний путь.");
+			Console.WriteLine("Выходи навстречу —");
+			Console.WriteLine("В высоте отдохнуть.");
+			Console.WriteLine("Полюби мои зори —");
+			Console.WriteLine("Знак моей любви.");
+			Console.WriteLine("И в свободное море");
+			Console.WriteLine("Навсегда плыви.");
+			Console.ReadKey();
+		}
+	}
+}
